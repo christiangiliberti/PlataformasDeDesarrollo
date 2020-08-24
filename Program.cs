2a89f56d2aa6358de988105304abeb9b84542f30
DESKTOP-1D10UFF\chris <christian.giliberti@davinci.edu.ar>
2020-08-24 19:22:37 -0300

    Create Program.cs
    Ejercicio_1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿ using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string mensaje;
+            Console.WriteLine("Escribe el primer numero");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escribe el segundo numero");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 >= num2){
+                mensaje = "El primer numero es mayor o igual que el segundo";
+                
+            }else{
+                mensaje = "El segundo numero es mayor que el primero";
+            }
+            Console.WriteLine(mensaje);
+            Console.ReadLine ();
+
+        }
+    }
+}
