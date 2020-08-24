a255c49116f4e24af20f8404fcbebfd934710f8c
DESKTOP-1D10UFF\chris <christian.giliberti@davinci.edu.ar>
2020-08-24 19:26:50 -0300

    Create Program2.cs
    Ejercicio_2

---
+++ Program2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, 
+            num = 0;
+            
+            Console.WriteLine("Se deben ingresar 10 numeros, el ingreso se detendra si la suma de los numero supera los 50");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ingrese numero");
+                num = int.Parse(Console.ReadLine());
+                sum += num;
+                if (sum >= 50)
+                {
+                break;
+                }
+            }
+
+        }
+    }
+}
