ac472e06989496ca1fd25b3f8b3a17650c63f576
DESKTOP-1D10UFF\chris <christian.giliberti@davinci.edu.ar>
2020-08-25 22:59:10 -0300

    20200825

---
+++ 20200825/ConsoleApp4/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            int n = rd.Next(1000), conInt = 0, num;
+            do
+            {
+                Console.Write("Ingrese número");
+                num = Convert.ToInt32(Console.ReadLine());
+                conInt++;
+                if (num > n) Console.WriteLine("El Numero es menor...");
+                else if (num < n) Console.WriteLine("El Numero es mayor...");
+            }
+            while (num != n);
+                    Console.WriteLine("Felicitaciones adivinaste en {0} intentos", conInt);
+                    Console.ReadKey();
+
+                }
+            }}
+        
+
