11831b167dad9b8283cc8f868d860725bef1abb0
DESKTOP-1D10UFF\chris <christian.giliberti@davinci.edu.ar>
2020-09-06 13:40:43 -0300

    20200901

--- 20200901/ConsoleApp1/ConsoleApp1/Program.cs
+++ 20200901/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,14 +6,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            /*
             Console.WriteLine("El juego consiste en adivinar un numero del 1 al 20), \ntenes 3 chances para hacerlo");
 
             Random random = new Random();
             int numSecreto = random.Next(20)+1;
             int chance = 0;
             int num = 0;
-
 
             while (num != numSecreto && chance < 3)
             {
@@ -45,8 +44,48 @@ namespace ConsoleApp1
                 {
                     Console.Write("Perdiste, el numero era el : " + num);
                 }
+            */
 
+
+            /*
+            Random random = new Random();
+            int secreto = random.Next(10);
+            int numero = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                IngreseElNumero("Ingrese el numero :", ref numero);
+                if (numero == secreto) break;
             }
+            string resultado;
+            if (numero == secreto)
+            {
+                resultado = Ganaste();
+            }
+            else
+            {
+                resultado = Perdiste(secreto);
+            }
+            Console.WriteLine(resultado);
+            }
+        static void IngreseElNumero(string mensaje, ref int numero)
+        {
+            Console.Write(mensaje);
+            string valor = Console.ReadLine();
+            numero = int.Parse(valor);
         }
+        static string Ganaste()
+        {
+            string mensaje = "Ganaste";
+            return mensaje;
+        }
+        static string  Perdiste(int valor)
+        {
+            string mensaje = "Perdiste el numero era " + valor;
+            return mensaje;
+        }
+        */
+
+
     }
-}
+    }
+
