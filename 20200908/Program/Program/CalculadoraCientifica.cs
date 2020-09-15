9be24c66cfa61c65f49cea6a6fb1d144480b128f
christiangiliberti <69557693+christiangiliberti@users.noreply.github.com>
2020-09-15 18:46:43 -0300

    .

---
+++ 20200908/Program/Program/CalculadoraCientifica.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    class CalculadoraCientifica : Calculadora // Herencia
+    {
+        public double Potencia(int valor, int exponente)
+        {
+            return Math.Pow(valor, exponente);
+        }
+
+    }
+}
