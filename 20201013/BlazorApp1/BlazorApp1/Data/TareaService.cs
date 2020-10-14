76db4657fdcf2a20c7add275771bf4c017cc6b83
christiangiliberti <69557693+christiangiliberti@users.noreply.github.com>
2020-10-13 23:00:36 -0300

    .

---
+++ 20201013/BlazorApp1/BlazorApp1/Data/TareaService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class TareaService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public BlazorApp1.Data.Tarea [] GetTareasAsync()
+        {
+            Tarea[] resultado = new Tarea[10];
+            resultado[0] = new Tarea ("1","Tarea 1",20200101,20200101,"2",true);
+
+            return resultado;
+        }
+    }
+}
